4401bd268d1a2174415be13d281cd30f5ed6cd3b
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-12-28 19:20:43 +0900

    :lipstick: Improves countdown display

--- src/components/atoms/TimerDisplay.tsx
+++ src/components/atoms/TimerDisplay.tsx
@@ -4,14 +4,24 @@ import { formatTimer } from '@/utils';
 
 export const TimerDisplay = (props: { value: number; target?: number }) => {
   const timerValue = createMemo(() => {
-    if (!props.target || props.target === 0) return props.value;
-    if (props.target > props.value) return props.target - props.value;
-    return props.value;
+    if (!props.target || props.target === 0)
+      return formatTimer(props.value).split(':');
+    if (props.target > props.value)
+      return formatTimer(props.target - props.value).split(':');
+    return formatTimer(props.value).split(':');
   });
 
   return (
     <span class="text-4xl font-bold tracking-widest font-mono">
-      {formatTimer(timerValue())}
+      <span class="countdown">
+        <span style={`--value: ${timerValue()[0]}`}></span>
+        <span class="sr-only">{timerValue()[0]}</span>
+      </span>
+      :
+      <span class="countdown">
+        <span style={`--value: ${timerValue()[1]}`}></span>
+        <span class="sr-only">{timerValue()[1]}</span>
+      </span>
     </span>
   );
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,20 @@
 /* eslint-disable */
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', 'plugins/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral: colors.neutral,
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/line-clamp'),
     require('@tailwindcss/aspect-ratio'),
+    require('daisyui'),
   ],
 };
