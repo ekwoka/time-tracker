d82031de8605edb3cef501a938c4c772144c6907
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2023-02-17 23:45:46 +0400

    :bug: Fixes Cross Compilation

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,26 +2,26 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-use tauri::{SystemTray, SystemTrayEvent};
+use tauri::SystemTray;
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 
 #[tauri::command]
-fn update_tray_title(title: &str, app_handle: tauri::AppHandle) {
-    app_handle.tray_handle().set_title(title).unwrap();
+fn update_tray_title(_title: &str, _app_handle: tauri::AppHandle) {
+  #[cfg(target_os = "macos")]
+  _app_handle.tray_handle().set_title(_title).unwrap();
 }
 
 fn main() {
-    let tray = SystemTray::new().with_title("Time Tracker");
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![update_tray_title])
-        .system_tray(tray)
-        .on_system_tray_event(|app, event| match event {
-          SystemTrayEvent::LeftClick { .. } => {
-            app.tray_handle().set_title("Tracking...").unwrap();
-          },
-          _ => {}
-        })
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  #[cfg(not(target_os = "macos"))]
+  let tray = SystemTray::new();
+
+  #[cfg(target_os = "macos")]
+  let tray = SystemTray::new().with_title("Time Tracker");
+
+  tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![update_tray_title])
+    .system_tray(tray)
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
