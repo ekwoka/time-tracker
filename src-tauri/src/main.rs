0375d27139cff2a28ecf67582c413b8bfae06051
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2024-03-11 16:40:41 +0900

    :arrow_up: Upgrades Everything

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,20 +8,20 @@ use tauri::SystemTray;
 
 #[tauri::command]
 fn update_tray_title(_title: &str, _app_handle: tauri::AppHandle) {
-  #[cfg(target_os = "macos")]
-  _app_handle.tray_handle().set_title(_title).unwrap();
+    #[cfg(target_os = "macos")]
+    _app_handle.tray_handle().set_title(_title).unwrap();
 }
 
 fn main() {
-  #[cfg(not(target_os = "macos"))]
-  let tray = SystemTray::new();
+    #[cfg(not(target_os = "macos"))]
+    let tray = SystemTray::new();
 
-  #[cfg(target_os = "macos")]
-  let tray = SystemTray::new().with_title("Time Tracker");
+    #[cfg(target_os = "macos")]
+    let tray = SystemTray::new().with_title("Time Tracker");
 
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![update_tray_title])
-    .system_tray(tray)
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![update_tray_title])
+        .system_tray(tray)
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
